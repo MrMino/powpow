79f6a56c80460c324ad6252f177a90ea9c7ba30b
Blazej Michalik <blazej.michalik@nokia.com>
2021-04-15 01:29:33 +0200

    grep: add more tests

--- tests/test_grep.py
+++ tests/test_grep.py
@@ -1,6 +1,16 @@
-from powpow import grep
+from powpow import grep, GrepResult
 
 
-def test_grep_a_string():
-    output = ("this is a string" | grep("string"))
-    assert bool(output) is True
+class TestGrep:
+
+    def test_grep_a_string(self):
+        output = ("this is a string" | grep("string"))
+        assert bool(output) is True
+
+    def test_results_in_GrepResult(self):
+        output = ("this is a string" | grep("string"))
+        assert isinstance(output, GrepResult)
+
+    def test_parameters_are_directly_available(self):
+        g = grep('pattern', highlight='highlight')
+        assert g.pattern == 'pattern' and g.highlight == 'highlight'
